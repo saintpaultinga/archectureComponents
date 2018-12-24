faa9de40f3ff43e5a1e11891cbacfb6b644bd826
Saint-Paul Tinga <saintpaultinga@gmail.com>
2018-12-24 00:10:49 -0500

    add equals and hashcode method in Note.
    Signed-off-by: Saint-Paul Tinga <saintpaultinga@gmail.com>

--- app/src/main/java/com/example/stinga/archecturecomponents/entity/Note.java
+++ app/src/main/java/com/example/stinga/archecturecomponents/entity/Note.java
@@ -4,6 +4,8 @@ package com.example.stinga.archecturecomponents.entity;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
+import java.util.Objects;
+
 @Entity(tableName = "note_table")
 public class Note {
     @PrimaryKey(autoGenerate = true)
@@ -37,5 +39,21 @@ public class Note {
 
     public int getPriority() {
         return priority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Note)) return false;
+        Note note = (Note) o;
+        return getPriority() == note.getPriority() &&
+                Objects.equals(getTitle(), note.getTitle()) &&
+                Objects.equals(getDescription(), note.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getTitle(), getDescription(), getPriority());
     }
 }
